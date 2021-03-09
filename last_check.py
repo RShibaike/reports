388cca4f16a32829b8885231511fbec23daf9821
Ryoya <nicochandaioh38@gmail.com>
2021-03-10 00:06:35 +0900

    url修正＋引用に変換、上付き文字など確認（前田さんとC2は要再確認）

--- last_check.py
+++ last_check.py
@@ -1,14 +1,21 @@
 cnt = 1
-with open('2.markdown_ori/13.md', mode='r', encoding='utf-8') as fi:
+with open('8.tex_fix/D2/D2.tex', mode='r', encoding='utf-8') as fi:
     for line in fi:
         if "<" in line:
-            print(cnt)
+            print("<:"+str(cnt))
         if ">" in line:
-            print(cnt)
+            print(">:"+str(cnt))
         if "textsuperscript" in line:
-            print(cnt)
+            print("tsup:"+str(cnt))
+        if "textsubscript" in line:
+            print("tsub:"+str(cnt))
+        if "quote" in line:
+            print("q:"+str(cnt))
+        if "http" in line:
+            print("http:"+str(cnt))
+        if "~" in line:
+            print("~"+str(cnt))
+        if "textasciitilde" in line:
+            print("～2"+str(cnt))
 
         cnt += 1
-
-引用あるかみる！！
-quate?
--- space_erase.py
+++ space_erase.py
@@ -1,7 +1,7 @@
-fo = open('3.text/6.txt', 'w', encoding='utf-8', newline='\n')
+fo = open('3.text/None.txt', 'w', encoding='utf-8', newline='\n')
 
 
-with open('2.markdown_ori/6.md', mode='r', encoding='utf-8') as fi:
+with open('2.markdown_ori/None.md', mode='r', encoding='utf-8') as fi:
     for line in fi:
         line = line.replace(">", "")
         line = line.lstrip("　")
